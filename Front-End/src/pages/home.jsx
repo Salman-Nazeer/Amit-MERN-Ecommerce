d312c5b96b5ee21b3b21984d1df63417b69a1776
Salman-Nazeer <nazeersalman716@gmail.com>
2024-12-16 18:05:59 -0500

    Banner Section done

--- Front-End/src/pages/home.jsx
+++ Front-End/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Categorylist from "../components/category-list";
+import BannerProduct from "../components/banner-product";
 
 const home = () => {
   return (
     <div>
       <Categorylist />
+      <BannerProduct />
     </div>
   );
 };
